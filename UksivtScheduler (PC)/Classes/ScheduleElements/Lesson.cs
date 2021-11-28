83a1b99c06f47f864dfb85cb390339d3d28a221d
Locked15 <79941113+Locked15@users.noreply.github.com>
2021-11-28 23:28:04 +0500

    Project initialized.

---
+++ UksivtScheduler (PC)/Classes/ScheduleElements/Lesson.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+/// <summary>
+/// Область с классом пары.
+/// </summary>
+namespace UksivtScheduler__PC_.Classes
+{
+    /// <summary>
+    /// Класс, представляющий сущность пары.
+    /// </summary>
+    public class Lesson
+    {
+        #region Область: Свойства.
+        /// <summary>
+        /// Свойство, содержащее номер пары.
+        /// </summary>
+        public Int32 Number { get; set; }
+
+        /// <summary>
+        /// Свойство, содержащее название пары.
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Свойство, содержащее имя преподавателя.
+        /// </summary>
+        public String Teacher { get; set; }
+
+        /// <summary>
+        /// Свойство, содержащее место проведения пары.
+        /// </summary>
+        public String Place { get; set; }
+        #endregion
+
+        #region Область: Конструкторы.
+        /// <summary>
+        /// Конструктор класса по умолчанию.
+        /// </summary>
+        public Lesson()
+        {
+
+        }
+
+        /// <summary>
+        /// Конструктор класса для заполнения пустой пары.
+        /// </summary>
+        /// <param name="number">Номер пары.</param>
+        public Lesson(Int32 number)
+        {
+            Number = number;
+            Name = null;
+            Teacher = null;
+            Place = null;
+        }
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        /// <param name="number">Номер пары.</param>
+        /// <param name="name">Названия пары.</param>
+        /// <param name="teacher">Имя преподавателя.</param>
+        /// <param name="place">Место проведения.</param>
+        public Lesson(Int32 number, String name, String teacher, String place)
+        {
+            Number = number;
+            Name = name;
+            Teacher = teacher;
+            Place = place;
+        }
+        #endregion
+    }
+}
---
+++ UksivtScheduler (PC)/Classes/ScheduleElements/WeekSchedule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Область с классом недельного расписания.
+/// </summary>
+namespace UksivtScheduler__PC_.Classes
+{
+    /// <summary>
+    /// Класс, представляющий сущность Расписания на неделю.
+    /// </summary>
+    public class WeekSchedule
+    {
+        #region Область: Свойста.
+        /// <summary>
+        /// Свойство, содержащее название группы.
+        /// </summary>
+        public String GroupName { get; set; }
+
+        /// <summary>
+        /// Свойство, содержащее расписание группы на неделю.
+        /// </summary>
+        public List<DaySchedule> Days { get; set; }
+        #endregion
+
+        #region Область: Конструкторы.
+        /// <summary>
+        /// Конструктор класса по умолчанию.
+        /// </summary>
+        public WeekSchedule()
+        {
+
+        }
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        /// <param name="groupName">Название группы.</param>
+        /// <param name="days">Список дней с расписанием.</param>
+        public WeekSchedule(String groupName,  List<DaySchedule> days)
+        {
+            GroupName = groupName;
+            Days = days;
+        }
+        #endregion
+    }
+}
